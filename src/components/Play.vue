512fca2f3553a6d574f6f6c4fc437dff37250c7e
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2017-11-29 22:29:27 +0700

    Halaman Homepage

---
+++ src/components/Play.vue
@@ -0,0 +1,26 @@
+<template>
+  <button type="button" @click="createRoom">Hai</button>
+</template>
+<script type="text/javascript">
+import * as firebase from "firebase";
+firebase.initializeApp({
+  apiKey: "AIzaSyDzFWZDmj4qjUEBpo8hXbFBow5zWQy1tWU",
+  authDomain: "group-project-week-3.firebaseapp.com",
+  databaseURL: "https://group-project-week-3.firebaseio.com",
+  projectId: "group-project-week-3"
+});
+export default{
+  data(){
+    return{
+      database:firebase.database()
+    }
+  },
+  methods:{
+    createRoom(){
+      console.log("Masuk");
+    }
+  }
+}
+</script>
+<style lang="scss">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import IndexHome from "@/components/IndexHome";
+import Play from "@/components/Play";
 
 Vue.use(Router)
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: IndexHome
+    },
+    {
+      path: "/play",
+      component: Play
     }
   ]
 })
